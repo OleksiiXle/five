be96d6a12a685b63597f9323d8539de996a8e364
oleksiXle <lokoko.xle@ukr.net>
2018-08-17 19:29:47 +0300

    2

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'amhGX_LsFjKXTaxgxNzyG9_lFPuvSHFJ',
+            'cookieValidationKey' => 'вапывап3453#$%^ываdghsdghh gfh ',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
